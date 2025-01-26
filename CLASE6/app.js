4c71183e35a2610517d6ef130ac0d076ce664016
CinthiaRod <Cinthiarb28@gmail.com>
2025-01-26 10:35:16 -0800

    Código ejercicio 3

--- CLASE6/app.js
+++ CLASE6/app.js
@@ -28,3 +28,32 @@ console.log(defaultGreeting, userName);
 console.log("Información sobre el sistema de ejecución del programa:");
 console.log("La platafora del sistema operativo es:", process.platform);//Imprimimos información sobre la plataforma
 console.log("El directorio actual es:", process.cwd())//imprimimos el directorio actual
+
+//Ejercicio 3: ¡Sumemos dos números!
+//Consigna: Tu tarea es escribir un programa interactivo que permita al usuario ingresar dos números y calcule su suma.
+// Este ejercicio te ayudará a practicar el uso del módulo readline para leer entradas del usuario en la consola.
+//Requisitos del programa:
+//Usa el módulo readline para crear una interfaz interactiva.
+//Usa readline.createInterface para manejar la entrada y salida del usuario.
+const rl = readline.createInterface({
+    input: process.stdin, //Entrada
+    output: process.stdout //Salida
+});
+
+//Pide al usuario que ingrese dos números, uno a la vez.
+//Utiliza callbacks para procesar las respuestas del usuario.
+rl.question ('Ingresa un número: ', (respuesta1) => {     
+    rl.question('Ingresa un segundo número: ', (respuesta2)=>{
+
+        //Calcula y muestra la suma de los dos números.
+        const sum = parseFloat(respuesta1) + parseFloat(respuesta2);
+        console.log(`La suma de ${respuesta1} + ${respuesta2} es: ${sum}`);
+        rl.close();//Cerramos readline después de recibir las respuestas del usuario
+    })
+});
+
+//Finaliza el programa con un mensaje de agradecimiento.
+rl.on('close', () =>{
+    console.log("¡Gracias por tus respuestas!");
+});
+
